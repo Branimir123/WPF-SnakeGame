2efbce6756b7bdb51c9ca968943fde15a548d153
Branimir <brani_svetoslavov@abv.bg>
2017-02-10 14:39:51 +0200

    Add constants class

---
+++ SnakeGame/SnakeGame/Constants/BaseConstants.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using SnakeGame.GameObjects.Common;
+using SnakeGame.GameObjects.Enums;
+
+namespace SnakeGame.Constants
+{
+    public static class BaseConstants
+    {
+        public const int MaxX = 1000;
+        public const int MaxY = 540;
+
+        public const int DefaultSnakeSize = 5;
+        public const int SquareSize = 15;
+        public const int DefaultSnakeSpeed = 15;
+        public const int FoodCount = 5;
+
+        public const int MovingFoodCount = 3;
+
+        public static readonly int MinObstaclesCount;
+        public static readonly int MaxObstaclesCount;
+        public static readonly int MinMovingObstaclesCount;
+        public static readonly int MaxMovingObstaclesCount;
+        public static readonly int MinMovingObstaclesSpeed;
+        public static readonly int MaxMovingObstaclesSpeed;
+        public static readonly int MinMovingObstaclesPrescaller;
+        public static readonly int MaxMovingObstaclesPrescaller;
+
+
+        public static readonly int MovingFoodsSpeed;
+
+
+        private static readonly Position SnakeStartPosition;
+
+        public static readonly Dictionary<Directions, int> DX;
+        public static readonly Dictionary<Directions, int> DY;
+
+        static BaseConstants()
+        {
+            DX = new Dictionary<Directions, int>
+            {
+                [Directions.Right] = 1,
+                [Directions.Left] = -1,
+                [Directions.Down] = 0,
+                [Directions.Up] = 0
+            };
+
+            DY = new Dictionary<Directions, int>
+            {
+                [Directions.Right] = 0,
+                [Directions.Left] = 0,
+                [Directions.Down] = 1,
+                [Directions.Up] = -1
+            };
+
+            MinObstaclesCount = 5;  //1 * ((MaxY * MaxX) / (SquareSize * SquareSize)) / 100;
+            MaxObstaclesCount = 10; //2 * ((MaxY * MaxX) / (SquareSize * SquareSize)) / 100;
+
+            MinMovingObstaclesCount = 5;
+            MaxMovingObstaclesCount = 10;
+
+            MinMovingObstaclesSpeed = 1;
+            MaxMovingObstaclesSpeed = DefaultSnakeSpeed / 2;
+
+            MovingFoodsSpeed = 7;
+
+            MinMovingObstaclesPrescaller = 20;
+            MaxMovingObstaclesPrescaller = 40;
+        }
+    }
+}
---
+++ SnakeGame/SnakeGame/GameObjects/Common/Position.cs
@@ -0,0 +1,97 @@
+﻿using SnakeGame.ViewModels;
+using SnakeGame.Constants;
+
+namespace SnakeGame.GameObjects.Common
+{
+    public class Position : BaseViewModel
+    {
+        private double x;
+        private double y;
+        public Position(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public double X
+        {
+            get
+            {
+                return this.x;
+
+
+            }
+            set
+            {
+                //if (value < 0)
+                //{
+                //    throw new ArgumentException("X cannot be less than 0", "x");
+                //}
+                //else if (value > BaseConstants.MaxX)
+                //{
+                //    throw new ArgumentException("X cannot be greater than Max X", "x");
+                //}
+
+                var newValue = value;
+                if (newValue < 0)
+                {
+                    newValue += BaseConstants.MaxX;
+                }
+                else if (newValue >= BaseConstants.MaxX)
+                {
+                    newValue %= BaseConstants.MaxX;
+                }
+                this.x = newValue;
+                this.OnPropertyChanged("X");
+            }
+        }
+
+        public double Y
+        {
+            get
+            {
+                return this.y;
+            }
+            set
+            {
+                //if (value < 0)
+                //{
+                //    throw new ArgumentException("Y cannot be less than 0", "y");
+                //}
+                //else if (value > BaseConstants.MaxY)
+                //{
+                //    throw new ArgumentException("Y cannot be greater than Max Y", "y");
+                //}
+                var newValue = value;
+                if (newValue < 0)
+                {
+                    newValue += BaseConstants.MaxY;
+                }
+                else if (newValue >= BaseConstants.MaxY)
+                {
+                    newValue %= BaseConstants.MaxY;
+                }
+
+                this.y = newValue;
+                this.OnPropertyChanged("Y");
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Position))
+            {
+                return false;
+            }
+
+            Position other = (Position)obj;
+
+            return this.X == other.x && this.Y == other.Y;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0}, {1})", this.x, this.y);
+        }
+    }
+}
