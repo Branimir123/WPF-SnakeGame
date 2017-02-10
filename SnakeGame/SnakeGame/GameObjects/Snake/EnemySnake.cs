e8cd7f9580a5d45c0741dceb4c333d84d2ac5cdb
Branimir <brani_svetoslavov@abv.bg>
2017-02-10 15:01:22 +0200

    Add enemy snake class

---
+++ SnakeGame/SnakeGame/GameObjects/Snake/EnemySnake.cs
@@ -0,0 +1,13 @@
+﻿using SnakeGame.GameObjects.Common;
+
+namespace SnakeGame.GameObjects.Snake
+{
+    public class EnemySnake : Snake
+    {
+        public EnemySnake(Position pos, int size, int speed)
+            : base(pos, size, speed)
+        {
+
+        }
+    }
+}
